78fedaafcdb9d9e4036d7e03d0377a4105dc7aa8
junwoo <junnukim1007@gmail.com>
2025-01-28 18:40:14 +0900

    요세푸스문제/실버4/15분

--- week01/BOJ_1158/요세푸스문제_김준우.java
+++ week01/BOJ_1158/요세푸스문제_김준우.java
@@ -10,25 +10,21 @@ public class 요세푸스문제_김준우 {
         Queue<Integer> queue = new LinkedList<>();
         List<Integer> list = new ArrayList<>();
 
-        // 입력 처리
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        // 큐에 값 추가 (1부터 n까지)
         for (int i = 1; i <= n; i++) {
             queue.add(i);
         }
 
-        // 요세푸스 순열 계산
         while (!queue.isEmpty()) {
             for (int i = 0; i < k - 1; i++) {
-                queue.add(queue.poll()); // k-1번 맨 앞의 값을 뒤로 보냄
+                queue.add(queue.poll());
             }
-            list.add(queue.poll()); // k번째 값을 리스트에 추가
+            list.add(queue.poll());
         }
 
-        // 결과 출력
         bw.write("<");
         for (int i = 0; i < list.size(); i++) {
             bw.write(String.valueOf(list.get(i)));
