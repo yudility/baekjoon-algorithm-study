808eb80f55c7f7064ff73a2900a7cbae7c93004a
yudility <yudility@gmail.com>
2025-02-05 09:24:52 +0900

    refactor: 부품대여장_연예림으로 변경

--- week02/BOJ_21942/부품대여장_연예림.java
+++ week02/BOJ_21942/부품대여장_연예림.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class 부품대여장 {
+public class 부품대여장_연예림 {
     static int[] days = calDays();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
