bc64dd4a77ea933b221ad15472e1b54314971f82
junwoo <junnukim1007@gmail.com>
2025-02-10 10:45:29 +0900

    에라토스테네스의체/실버4/김준우

--- week02/BOJ_11279/최대힙_민기.java
+++ week02/BOJ_11279/최대힙_민기.java
@@ -1,15 +1,9 @@
 package BOJ_11279;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.io.*;
 
 public class 최대힙_민기 {
-
-    public 최대힙_민기() {
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
