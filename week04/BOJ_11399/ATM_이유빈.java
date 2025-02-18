2765709b67d774ff20a4a56022f20eb2f9b0c161
yubin8773 <hiyubin.lee@gmail.com>
2025-02-18 11:13:08 +0900

    ATM / 실버4 / 18분 / O

---
+++ week04/BOJ_11399/ATM_이유빈.java
@@ -0,0 +1,33 @@
+package BOJ_11399;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class ATM_이유빈 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        arr = Arrays.stream(arr).sorted().toArray();
+
+        int result = 0;
+
+        for (int i=0; i<n; i++) {
+            int tmp = 0;
+            for (int k=0; k<=i; k++) {
+                tmp += arr[k];
+            }
+            result += tmp;
+        }
+        bw.write(result + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
