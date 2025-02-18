1e8d2d2c9655f967b9db1f318379ed1cbf72e5bf
yubin8773 <hiyubin.lee@gmail.com>
2025-02-18 10:43:07 +0900

    거스름돈 / 실버5 / 26분 / O

---
+++ week04/BOJ_14916/거스름돈_이유빈.java
@@ -0,0 +1,29 @@
+package BOJ_14916;
+
+import java.io.*;
+
+public class 거스름돈_이유빈 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n < 5 && n % 2 != 0) {
+            bw.write(-1+"");
+        }
+
+        int k = n / 5;
+        int m = n % 5;
+
+        if (m % 2 == 0) {
+            bw.write((m/2 + k) + "");
+        } else {
+            bw.write(((k-1) + (m+5)/2) + "");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
