15b2388f2d5537bc66813d72ce3c99d2e1c64e42
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-03-11 19:28:59 +0900

    피아노체조 / 실버1 / 15분 / O

--- week07/BOJ_21318/피아노체조_연예림.java
+++ week07/BOJ_21318/피아노체조_연예림.java
@@ -1,5 +1,47 @@
 package BOJ_21318;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class 피아노체조_연예림 {
 
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+    int N = Integer.parseInt(br.readLine());
+
+    int[] paper = new int[N+1];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 1; i <=N; i++) {
+      paper[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int[] check = new int[N+1];
+    for (int i = 1; i <N; i++) {
+      if (paper[i] > paper[i+1]) {
+        check[i] = check[i-1] + 1;
+      } else {
+        check[i] = check[i-1];
+      }
+    }
+    check[N] = check[N-1];
+
+    int Q = Integer.parseInt(br.readLine());
+    for (int i = 1; i <=Q; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+
+      int minus = 0;
+      if (y+1 < paper.length && paper[y] > paper[y+1]) {
+        minus = 1;
+      }
+      sb.append(check[y] - check[x-1] - minus).append("\n");
+    }
+    System.out.println(sb);
+  }
 }
