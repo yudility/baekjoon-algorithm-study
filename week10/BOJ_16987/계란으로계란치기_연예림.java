e5d25867c32a656ce22598bf07125533f03af6f4
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-04-08 23:47:05 +0900

    fix: 풀이성공

--- week10/BOJ_16987/계란으로계란치기_연예림.java
+++ week10/BOJ_16987/계란으로계란치기_연예림.java
@@ -42,33 +42,33 @@ public class 계란으로계란치기_연예림 {
       return;
     }
 
-    boolean target = false; // 깰 계란이 있는지 확인
-    if (eggs[idx][0] > 0) {   // 손에 든 달걀이 깨져있는지
-      for (int i = 0; i < N; i++) { // 오른쪽 달걀부터 치기x => 모든 달걀 치기
+    if (eggs[idx][0] <= 0) { // 손에 든 달걀이 깨져있으면 다음 달걀 들기
+      getEggMax(idx + 1, cnt);
+      return;
+    }
 
-        if (i == idx || eggs[i][0] <= 0) continue; // 이미 깨진 달걀, 내 달걀을 치지 않음
+    boolean target = false; // 다른 달걀이 모두 깨진 경우를 고려
+    for (int i = 0; i < N; i++) { // 모든 달걀 중 하나치기
 
-        target = true;
+      if (i == idx || eggs[i][0] <= 0) continue; // 이미 깨진 달걀, 내 달걀은 치지 않음
 
-        eggs[i][0] -= eggs[idx][1];
-        eggs[idx][0] -= eggs[i][1];
+      target = true;
 
-        int check = 0; // 깨진 달걀 수
-        if (eggs[i][0] < 0) check++;
-        if (eggs[idx][0] < 0) check++;
-        getEggMax(idx + 1, cnt + check);
+      eggs[i][0] -= eggs[idx][1];
+      eggs[idx][0] -= eggs[i][1];
 
-        // 백트래킹
-        eggs[i][0] += eggs[idx][1];
-        eggs[idx][0] += eggs[i][1];
+      int check = 0; // 깨진 달걀 수
+      if (eggs[i][0] <= 0) check++; // 등호 조건은 빼먹었었음.
+      if (eggs[idx][0] <= 0) check++;
+      getEggMax(idx + 1, cnt + check);
 
-      }
+      // 백트래킹
+      eggs[i][0] += eggs[idx][1];
+      eggs[idx][0] += eggs[i][1];
 
-      if (!target || idx == N-1) { // 깰 계란이 없거나 마지막 계란인 경우
-        getEggMax(idx + 1, cnt);
-      }
+    }
 
-    } else { // 손에 든 달걀이 깨져있으면 다음 달걀 들기
+    if (!target || idx == N-1) { // 깰 계란이 없거나 마지막 계란인 경우
       getEggMax(idx + 1, cnt);
     }
 
