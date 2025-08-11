9dc3d5de3516bd56e07c9eb391c45d2d77418848
yudility <yudility@gmail.com>
2025-08-11 21:17:07 +0900

    바이러스 / 실버3 / 50분 / X

---
+++ re01/BOJ_2606/바이러스_박유진.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+
+	public static void main(String[] args) throws IOException {
+
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+
+	    int n = Integer.parseInt(br.readLine());
+	    int v = Integer.parseInt(br.readLine());
+
+	    ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+	    int[] visited = new int[n];
+
+	    for (int i = 0 ; i < n ; i++){
+		    visited[i] = 0;
+	    }
+
+	    for (int i = 0 ; i < n ; i++){
+	        list.add(new ArrayList<>());
+	    }
+
+	    for(int i = 0; i < v; i++){
+	       st = new StringTokenizer(br.readLine());
+	       int a = Integer.parseInt(st.nextToken());
+	       int b = Integer.parseInt(st.nextToken());
+	       list.get(a-1).add(b-1);
+	       list.get(b-1).add(a-1);
+	   }
+
+	   int answer = 0;
+
+	   Queue<Integer> q = new LinkedList<>();
+
+	   q.add(0);
+	   visited[0] = 1;
+
+	   while(!q.isEmpty()){
+	       int now = q.poll();
+
+	       for(int node : list.get(now)){
+			       if(visited[node] != 1){
+			         q.add(node);
+		           visited[node] = 1;
+		           answer++;
+			       }
+	       }
+	   }
+	   System.out.println(answer);
+	}
+}
